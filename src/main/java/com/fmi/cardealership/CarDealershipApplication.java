51760d80f4000b73ee5e0e39a129367186b398e4
YoanTsvyatkov <YoanTsvyatkov@gmail.com>
2022-05-09 16:03:06 +0300

    Added libraries and db setup

---
+++ src/main/java/com/fmi/cardealership/CarDealershipApplication.java
@@ -0,0 +1,13 @@
+package com.fmi.cardealership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipApplication.class, args);
+	}
+
+}
