e2e74b572a5c2d349f692805b7c807f236fff851
Ivo <ivomarinov3333@gmail.com>
2022-06-22 10:07:21 +0300

    added register page

--- client/src/pages/register/register.js
+++ client/src/pages/register/register.js
@@ -1,7 +1,107 @@
-import React from 'react'
+import React from "react";
+import { Form, Button, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import useForm from "../../custom-hooks/use-form";
+import validate from "../../utils/register-form-validation";
+import styles from "./register.module.css"
 
-export default function Register() {
+export default function Registration() { 
+  const navigate = useNavigate();
+
+  const onRegFormSubmit = async () => {
+    values.role = 'USER'
+    try {
+      const response = await fetch(`http://localhost:8080/api/register`, {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers : {
+          "Content-Type" : "application/json"
+        }
+      });
+
+      if(response.status !== 200) {
+        //???
+      }
+
+      const data = await response.json();
+     
+    } catch(err) {
+      console.log(err)
+    }
+    navigate('/login')
+  }
+
+  
+  const { values, errors, handleSubmit, handleChange } = useForm(
+    onRegFormSubmit,
+    validate
+  );
+
   return (
-    <div>Register</div>
-  )
+    <>
+        <Form onSubmit={handleSubmit} className={styles.container}>
+          <h3>Register</h3>
+          <Row>
+              <Form.Group className="mb-3" controlId="formGroupName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" isInvalid={!!errors.name} onChange={handleChange} name="name" value={values.name || ''} />
+                <Form.Control.Feedback type="invalid">
+                  {errors.name}
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group className="mb-3" controlId="formGroupUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" isInvalid={!!errors.username} onChange={handleChange} name="username" value={values.username || ''}/>
+                <Form.Control.Feedback type="invalid">
+                  {errors.username}
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+          <Row>
+              <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" isInvalid={!!errors.password} onChange={handleChange} name="password" value={values.password || ''} />
+                <Form.Control.Feedback type="invalid">
+                  {errors.password}
+                </Form.Control.Feedback>
+             </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" name="email" isInvalid={!!errors.email} onChange={handleChange} value={values.email || ''}/>
+              </Form.Group>
+              <Form.Control.Feedback type="invalid">
+                  {errors.email}
+                </Form.Control.Feedback>
+            </Row>
+          <Row>
+              <Form.Group className="mb-3" controlId="formGroupBirthdate">
+                <Form.Label>Date of Birth</Form.Label>
+                <Form.Control type="date" isInvalid={!!errors.date} name="date" onChange={handleChange} value={values.date || undefined}/>
+                <Form.Control.Feedback type="invalid">
+                  {errors.date}
+                </Form.Control.Feedback>
+              </Form.Group>
+          </Row>
+          <Row>
+              <Form.Group className="mb-3" controlId="formGroupPhoneNumber">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control type="text" isInvalid={!!errors.phoneNumber} name="phoneNumber" onChange={handleChange} value={values.phoneNumber || ''} />
+                <Form.Control.Feedback type="invalid">
+                  {errors.phoneNumber}
+                </Form.Control.Feedback>
+              </Form.Group>
+          </Row>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+    </>
+  );
 }
+
+
+
---
+++ client/src/utils/register-form-validation.js
@@ -0,0 +1,42 @@
+export default function validate(values) {
+    let errors = {}
+
+    if(!values.name) {
+        errors.name = "Name must not be empty"
+    }
+
+    if(!values.username) {
+        errors.username = "Username must not be empty"
+    }
+
+    if(values.username && values.username.length < 6) {
+        errors.username = "Username must be at least 6 characters"
+    }
+
+    
+    if(!values.password) {
+        errors.password = "Password must not be empty"
+    }
+
+    if(values.password && values.password.length < 6) {
+        errors.password = "Password must be at least 6 characters"
+    }
+
+    if(!values.date) {
+        errors.date = "Date of birth must not be empty"
+    }
+
+    if(!values.email) {
+        errors.email = "Email must not be empty"
+    }
+
+    if(!values.phoneNumber) {
+        errors.phoneNumber = "Phone number must not be empty"
+    }
+
+    if(values.phoneNumber && values.phoneNumber.length !== 10) {
+        errors.phoneNumber = "Phone number must be 10 digits long"
+    }
+
+    return errors;
+}
